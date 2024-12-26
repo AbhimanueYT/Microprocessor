6660180f220b46eff371b8b549ad4ad76d0682ad
Abhimanue T D <103575176+AbhimanueYT@users.noreply.github.com>
2024-12-26 19:32:01 +0530

    Update square.asm

--- 8086/square.asm
+++ 8086/square.asm
@@ -7,8 +7,9 @@ stack ends
 data segment
 	m db 10,13,"Enter value $"
 	r db 10,13,"Result $"
-	num1 db 20("$")
-	num2 db 20("$")
+	num1 db 0
+	num2 db 0
+	re dw 0
 data ends
 
 code segment
@@ -23,35 +24,7 @@ code segment
 		mov ah,01h
 		int 21h
 		sub al,'0'
-		
-		mul al
-		mov bx,ax
-		
-		lea dx,r
-		mov ah,09h
-		int 21h
-		
-		mov ax,bx
-		
-		mov cx,0
-		mov bx,10
-	d:	mov dx,0
-		div bx
-		push dx
-		inc cx
-		test ax,ax
-		jnz d 	
-		
-		mov cx,0
-	p:	pop dx
-		add dl,'0'
-		mov num1[cx],dl
-		inc cx
-		loop p		
-		
-		lea dx,num1
-		mov ah,09h
-		int 21h
+		mov num1,al
 		
 		lea dx,m
 		mov ah,09h
@@ -63,31 +36,31 @@ code segment
 		
 		mul al
 		mov bx,ax
+		mov al,num1
+		mul al
+		add ax,bx
+		mov re,ax
 		
 		lea dx,r
 		mov ah,09h
 		int 21h
 		
-		mov ax,bx
-		
+		mov ax,re
 		mov cx,0
 		mov bx,10
-	d1:	mov dx,0
+	d:	mov dx,0
 		div bx
 		push dx
 		inc cx
 		test ax,ax
-		jnz d1
+		jnz d
 		
-	p1:	pop dx
+		
+	p:	pop dx
 		add dl,'0'
-		mov num2,dl
-		loop p1			
-		
-		lea dx,num2
-		mov ah,09h
+		mov ah,02h
 		int 21h
-			
+		loop p
 		
 		mov ah,4ch
 		int 21h
