f8efc68d4291f0bccaafb3581507b432d6d48679
Abhimanue T D <103575176+AbhimanueYT@users.noreply.github.com>
2025-01-04 12:05:53 +0530

    Update fibonacci.asm

--- 8085/fibonacci.asm
+++ 8085/fibonacci.asm
@@ -1,1 +1,22 @@
+lxi h,5000h
+mov c,m
+mvi a,0
+inx h
+mov m,a
+mvi b,1
+mov d,a
+add b
+mov b,d
+mov d,a
+call fab
+hlt
 
+fab:	inx h
+	mov m,d
+    mov d,a
+	add b
+    mov b,d
+    mov d,a
+	dcr c
+	jnz fab
+	ret
