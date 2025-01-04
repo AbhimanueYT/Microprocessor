11b7c8a07977c10c6f49b6782a2b78ccdb8046e4
Abhimanue T D <103575176+AbhimanueYT@users.noreply.github.com>
2025-01-04 19:17:09 +0530

    Update prime.asm

--- 8085/prime.asm
+++ 8085/prime.asm
@@ -1,1 +1,26 @@
-
+lxi h,5000h
+mov d,m
+loop: inx h
+      mov a,m
+      mvi e,2
+      cmp e
+      jc exit
+      jz exit1
+div:  sub e 
+      cmp e
+      jz exit
+      jc ne
+      jmp div
+ne:   call next
+      jmp exit1
+exit: mvi m,00
+exit1: dcr d
+      jnz loop
+      hlt
+next: mov a,m
+      inr e
+      cmp e
+      jc exit
+      jz exit1
+      jmp div
+ret
