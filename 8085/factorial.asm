39bbf47c1e50c58c5508e18a43a5b8fb1e88d1be
Abhimanue T D <103575176+AbhimanueYT@users.noreply.github.com>
2025-01-04 13:09:17 +0530

    Update factorial.asm

--- 8085/factorial.asm
+++ 8085/factorial.asm
@@ -1,1 +1,21 @@
+lxi h,5000h
+mov a,m
+mov d,a
+mov b,a
+  fac: mov c,b
+       dcr c
+       jz last
+       dcr c
+       call MUL
+ last: dcr b
+       jnz fac
+inx h
+mov m,a
+hlt
 
+MUL: jz exit
+     add d
+     dcr c
+     jnz MUL
+     mov d,a
+exit: ret
